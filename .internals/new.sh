f36b4c2033387495030770eaf5557c0244f88219
Ruan Bekker <ruan.ru.bekker@gmail.com>
2024-07-15 10:24:21 +0200

    :sparkles: add template script

---
+++ .internals/new.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=""
+
+if [[ ! -n "${PROJECT_NAME}" ]]
+  then
+    echo "PROJECT_NAME argument is missing"
+    exit 1
+fi
+
+if [[ -d "${PROJECT_NAME}" ]]
+  then
+    echo "project already exists"
+    exit 1
+fi
+
+mkdir -p ${PROJECT_NAME}
+
+cat > "${PROJECT_NAME}/docker-compose.yaml" << EOF
+version: '3.8'
+
+services:
+  $PROJECT_NAME:
+    image: replace_tag
+    container_name: $PROJECT_NAME
+    restart: unless-stopped
+    ports:
+      - replace_sourceport:replace_targetport
+    environment:
+      - RB_PORT=replace_sourceport
+    networks:
+      - public
+
+networks:
+  public:
+    name: public
+EOF
